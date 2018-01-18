56158f0c50005e3955745e9f8c2f272015bb6cce
Rajat Narula <boyrajat@hotmail.com>
2018-01-18 14:53:36 -0500

    Semi-final push before bonus

---
+++ ClozeCard.js
@@ -0,0 +1,16 @@
+
+var ClozeCard = function(text, cloze) {
+	
+	var textToLower = text.toLowerCase();
+	var clozeToLower = text.toLowerCase();
+
+	 if (!textToLower.includes(clozeToLower)) {
+	 	console.log('ERROR - Cloze-Deletion does not appear within full text --- <' + cloze + '>');
+	 	return;
+	 }
+	 this.full = text;
+	 this.cloze = cloze;
+	 this.partial = text.replace(cloze, '........');
+	 }
+
+module.exports = ClozeCard;
---
+++ flashCardGame.js
@@ -0,0 +1,134 @@
+
+var inquire = require("inquirer");
+
+var BasicFlashCard = require("./BasicCard.js");
+var ClozeFlashCard = require("./ClozeCard.js");
+//var BQuestions = require("./questions.js");
+
+var BasicQuestions = [];
+var ClozeQuestions = [];
+//console.log(Questions);
+var bquestion1 = new BasicFlashCard("Which US president has the lowest approval ratings in history ?", "Donald Trump");
+BasicQuestions.push(bquestion1);
+var bquestion2 = new BasicFlashCard("Which planet is closest to the sun ?", "Mercury");
+BasicQuestions.push(bquestion2);
+var bquestion3 = new BasicFlashCard("What does Frank Hernandez like more than being with us cool kids ?", "Capital One");
+BasicQuestions.push(bquestion3);
+
+var cquestion1 = new ClozeFlashCard("Donald Trump has the lowest approval ratings for president in US History.", "Donald Trump");
+ClozeQuestions.push(cquestion1);
+var cquestion2 = new ClozeFlashCard("Mercury is the planet closest to the sun.", "Mercury");
+ClozeQuestions.push(cquestion2);
+var cquestion3 = new ClozeFlashCard("Frank Hernandez likes Capital One more than being with us cool kids.", "Capital One");
+ClozeQuestions.push(cquestion3);
+
+inquire.prompt({
+	type: "list",
+	message: "Please select what would you like to do :" + "\n",
+	choices: ["Learn with Basic Flash Cards", "Test Yourself With Cloze-Deleted Flash Cards"],
+	name: "userChoice"
+})
+.then(function(inquirerResponse) {
+	console.log("\nYou chose to: ->" + inquirerResponse.userChoice + "\n");
+
+	if (inquirerResponse.userChoice === "Learn with Basic Flash Cards") {
+		runBasicFlashCard();
+	} else if (inquirerResponse.userChoice === "Test Yourself With Cloze-Deleted Flash Cards") {
+		runClozeFlashCard();
+	}
+})
+//console.log(uestions.length);
+var currentQuestion = 0;
+
+function runBasicFlashCard() {
+	
+	if (currentQuestion >= BasicQuestions.length) {
+
+		inquire.prompt({
+			type: "confirm",
+			message: "\nEnough Practice, Lets play the game with Cloze-Deleted Flash Cards..." +
+			"\n" + "Ready ?",
+			name: "confirm",
+			default: true
+		})
+		.then(function(inquirerResponse) {
+			if (inquirerResponse.confirm) {
+				currentQuestion = 0;
+				runClozeFlashCard();
+			} else {
+				console.log("\n-----GOOD BYE-----");
+			}
+		})
+
+
+	} else if (currentQuestion < BasicQuestions.length) {
+	
+	inquire.prompt({
+		type: "confirm",
+		message: "\nQuestion: " + BasicQuestions[currentQuestion].front + "\n" +
+				"Answer: " + BasicQuestions[currentQuestion].back + "\n" +
+				"\nMove on? ",
+		name: "confirm",
+		default: true
+		
+		
+	})
+	.then(function(inquirerResponse) {
+		if (inquirerResponse.confirm) {
+			currentQuestion++;
+			runBasicFlashCard();
+
+		}
+	})
+	
+}
+}
+
+var clozeQuestion = 0;
+var correctAnswers = 0;
+var wrongAnswers = 0;
+
+function runClozeFlashCard() {
+	
+	if (clozeQuestion >= ClozeQuestions.length) {
+
+		console.log("\n<-_*-_*-_*-_*-_*-_*-__*-_*-_*-_GAME OVER_-*_-*_-*_-*_-*_-*__-*_*-_*-_*->");
+		console.log("\n~~~~~~ RESULTS: Corrects Answers: "+ correctAnswers + " & Incorrect Answers: " + wrongAnswers + " ~~~~~~\n");
+		
+
+
+	} else if (clozeQuestion < ClozeQuestions.length) {
+	
+	inquire.prompt({
+		type: "input",
+
+		message: "\nQuestion: " + ClozeQuestions[clozeQuestion].partial + "\n" +
+				"Answer: ", 
+				
+		name: "answer",
+		//default: true
+		
+		
+	})
+	.then(function(inquirerResponse) {
+
+		var answerToLowerCase = inquirerResponse.answer.toLowerCase();
+		var clozeToLowerCase = ClozeQuestions[clozeQuestion].cloze.toLowerCase();
+
+		if (answerToLowerCase === clozeToLowerCase) {
+			
+			console.log("\n{ GOOD WORK }");
+			clozeQuestion++;
+			correctAnswers++
+			runClozeFlashCard();
+
+		} else {
+			console.log("\n% YOU SUCK. %");
+			clozeQuestion++;
+			wrongAnswers++
+			runClozeFlashCard();
+		}
+	})
+	
+}
+}
